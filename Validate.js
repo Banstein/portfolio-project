b51ca2d5826c3e3ce29132375e0794e2f11c2b27
Vana Seraydarian <vanaseraydarian123@gmail.com>
2021-12-16 10:12:36 +0200

    Update index.html contact form and validate.js

--- Validate.js
+++ Validate.js
@@ -1,12 +1,10 @@
-const form = document.querySelector('.contacts-form');
+const form = document.querySelector('.contact-info');
 const EMAIL_LOWERCASE = 'Please email should be lowercase characters *';
+const msg = document.querySelector('.error-message');
 
 function showMessage(message) {
-  const msg = document.querySelector('.error-msg');
-  msg.innerText = message;
-//   input.className = type ? 'success' : 'error';
-//   return type;
-// }
+  msg.innerHTML = message;
+}
 
 function showError(input, message) {
   return showMessage(input, message, false);
@@ -15,15 +13,16 @@ function showError(input, message) {
 function validateEmail(input, invalidMsg) {
   const email = input;
   if (email !== email.toLowerCase()) {
-    return showError(input, invalidMsg)
+    return showError(invalidMsg);
   }
   return true;
 }
 form.addEventListener('submit', (event) => {
   event.preventDefault();
-  const emailValid = validateEmail(form.nextElementSibling.user_email, EMAIL_LOWERCASE);
+  const emailValid = validateEmail(document.querySelector('#user_email').value, EMAIL_LOWERCASE);
   if (emailValid) {
     form.submit();
+    msg.innerHTML = '';
     form.reset();
   }
-})
+});
